eac6d2bcb9c10f2cb7de7c77111a4adb0e4c6861
mathnap2 <143150021+mathnap2@users.noreply.github.com>
2026-09-17 21:21:53 -0600

    g

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,6 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World! from marhias napoles");
+		return new Response("Hello World! from mathias napoles");
 	},
 } satisfies ExportedHandler<Env>;
